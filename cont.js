dae3f6323b0fbef0043cc2927f103bd89e9ea2cb
Tigran Oganesyan <66678052+javaCoder1707@users.noreply.github.com>
2020-06-29 16:23:51 +0400

    first commit

---
+++ cont.js
@@ -0,0 +1,7 @@
+function initMap() {
+  let  map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 40.177387, lng:  44.526272},
+      scrollwheel: true,
+      zoom: 10
+    });
+  }
